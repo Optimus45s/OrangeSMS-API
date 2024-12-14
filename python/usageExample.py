c53bb1dae64bf3e123ca23cb92e06200d4531a49
Optimus45s <76058325+Optimus45s@users.noreply.github.com>
2024-12-14 00:09:05 +0000

    Update usageExample.py

--- python/usageExample.py
+++ python/usageExample.py
@@ -1,10 +1,28 @@
-from orangeSMS import sendSMS, getToken
+from orangeSMS import OrangeSMSAPI
 
 
-clientID = "aklGYUdsYjdhY2VpM...Hkxdg=="
-token = getToken(clientID)
-senderAdress = "+2210000"
-address = "+221781234567"
+client_id = "<your_client_id>"
+client_secret = "<your_client_secret_code>"
 
-result = sendSMS(senderAdress, address, "Salam from python", token)
-print(result)
+api = OrangeSMSAPI(client_id, client_secret)
+
+try:
+    token = api.get_token()
+    print("Access Token:", token)
+
+    sender = "+224611"
+    receiver = "+224611"
+    message = "Salam from Guinea"
+
+    response = api.send_sms(sender, receiver, message)
+    print("SMS Response:", response)
+
+
+    stats = api.get_usage_stats()
+    print("Usage Stats:", stats)
+
+    balance = api.show_balance_sms()
+    print("SMS Balance:", balance)
+
+except Exception as e:
+    print("Error:", str(e))
